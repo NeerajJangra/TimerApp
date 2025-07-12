6a31a73b462261174cfbf5326e605f5e6eb4bd89
Neeraj Jangra <neeraj.jangra@grapecity.com>
2025-07-12 16:22:22 +0530

    adding navigation and screens

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,12 +1,29 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import HistoryScreen from './screens/HistoryScreen';
+import HomeScreen from './screens/HomeScreen';
 
-const AppNavigator = () => {
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigator() {
   return (
-    <View>
-      <Text>AppNavigator</Text>
-    </View>
-  );
-};
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') iconName = 'home';
+            else if (route.name === 'History') iconName = 'time';
 
-export default AppNavigator;
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="History" component={HistoryScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
